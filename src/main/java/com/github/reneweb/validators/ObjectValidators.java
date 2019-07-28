f59d14b0f8ad70258c85d9219d2e3d3fc6ab0431
Rene Weber <weber.rene@live.de>
2019-07-28 19:11:02 +0100

    add object isInstanceOf

--- src/main/java/com/github/reneweb/validators/ObjectValidators.java
+++ src/main/java/com/github/reneweb/validators/ObjectValidators.java
@@ -4,15 +4,23 @@ import com.github.reneweb.ValidationResult;
 
 public interface ObjectValidators {
   default ValidationResult<Object> isNotNull(Object value) {
-    if (value != null) {
-      return ValidationResult.success(value);
-    } else {
-      return ValidationResult.failure(value, "Value is null");
-    }
+    return isNotNull(value, "Value is null");
   }
 
   default ValidationResult<Object> isNotNull(Object value, String invalidMessage) {
     if (value != null) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, invalidMessage);
+    }
+  }
+
+  default ValidationResult<Object> isInstanceOf(Object value, Class c) {
+    return isInstanceOf(value, c, "Value is not a instance of " + c);
+  }
+
+  default ValidationResult<Object> isInstanceOf(Object value, Class c, String invalidMessage) {
+    if (c.isInstance(value)) {
       return ValidationResult.success(value);
     } else {
       return ValidationResult.failure(value, invalidMessage);
