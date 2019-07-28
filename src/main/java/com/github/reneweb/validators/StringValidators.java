0cb21172dc652cb37f75b3e3edb49689f2c6ec37
Rene Weber <weber.rene@live.de>
2019-07-28 18:50:13 +0100

    simplify validators

--- src/main/java/com/github/reneweb/validators/StringValidators.java
+++ src/main/java/com/github/reneweb/validators/StringValidators.java
@@ -5,11 +5,7 @@ import com.github.reneweb.ValidationResult;
 public interface StringValidators {
 
   default ValidationResult<String> isNullOrEmpty(String value) {
-    if (value != null && !value.isEmpty()) {
-      return ValidationResult.success(value);
-    } else {
-      return ValidationResult.failure(value, "Value is null or empty");
-    }
+    return isNullOrEmpty(value, "Value is null or empty");
   }
 
   default ValidationResult<String> isNullOrEmpty(String value, String invalidMessage) {
@@ -21,11 +17,7 @@ public interface StringValidators {
   }
 
   default ValidationResult<String> hasLength(String value, int length) {
-    if (value.length() >= length) {
-      return ValidationResult.success(value);
-    } else {
-      return ValidationResult.failure(value, "Value is smaller than " + length);
-    }
+    return hasLength(value, length, "Value is smaller than " + length);
   }
 
   default ValidationResult<String> hasLength(String value, int length, String invalidMessage) {
