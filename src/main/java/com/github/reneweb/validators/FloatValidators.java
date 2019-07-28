844543851753058f83363016d90e0e07cb25091b
Rene Weber <weber.rene@live.de>
2019-07-28 20:07:33 +0100

    add double validators

---
+++ src/main/java/com/github/reneweb/validators/FloatValidators.java
@@ -0,0 +1,44 @@
+package com.github.reneweb.validators;
+
+import com.github.reneweb.ValidationResult;
+
+public interface FloatValidators {
+
+  default ValidationResult<Float> isEqual(Float value, Float toEqual) {
+    return isEqual(value, toEqual, "Value '" + value + "' is not equal " + toEqual);
+  }
+
+  default ValidationResult<Float> isEqual(Float value, Float toEqual, String invalidMessage) {
+    if (value.equals(toEqual)) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, invalidMessage);
+    }
+  }
+
+  default ValidationResult<Float> isGreaterThan(Float value, Float other) {
+    return isEqual(value, other, "Value '" + value + "' is not greater than " + other);
+  }
+
+  default ValidationResult<Float> isGreaterThan(Float value, Float other,
+                                                 String invalidMessage) {
+    if (value > other) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, invalidMessage);
+    }
+  }
+
+  default ValidationResult<Float> isSmallerThan(Float value, Float other) {
+    return isEqual(value, other, "Value '" + value + "' is not smaller than " + other);
+  }
+
+  default ValidationResult<Float> isSmallerThan(Float value, Float other,
+                                                 String invalidMessage) {
+    if (value < other) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, invalidMessage);
+    }
+  }
+}
