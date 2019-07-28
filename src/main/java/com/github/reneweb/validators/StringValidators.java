d374f462c613f5a6b218e2434cf1c8211d92cf4b
Rene Weber <weber.rene@live.de>
2019-07-28 18:48:06 +0100

    add string hasLength

--- src/main/java/com/github/reneweb/validators/StringValidators.java
+++ src/main/java/com/github/reneweb/validators/StringValidators.java
@@ -1,4 +1,38 @@
 package com.github.reneweb.validators;
 
-public class StringValidators {
+import com.github.reneweb.ValidationResult;
+
+public interface StringValidators {
+
+  default ValidationResult<String> isNullOrEmpty(String value) {
+    if (value != null && !value.isEmpty()) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, "Value is null or empty");
+    }
+  }
+
+  default ValidationResult<String> isNullOrEmpty(String value, String invalidMessage) {
+    if (value != null && !value.isEmpty()) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, invalidMessage);
+    }
+  }
+
+  default ValidationResult<String> hasLength(String value, int length) {
+    if (value.length() >= length) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, "Value is smaller than " + length);
+    }
+  }
+
+  default ValidationResult<String> hasLength(String value, int length, String invalidMessage) {
+    if (value.length() >= length) {
+      return ValidationResult.success(value);
+    } else {
+      return ValidationResult.failure(value, invalidMessage);
+    }
+  }
 }
