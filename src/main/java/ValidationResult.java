6256c663b558b86dfaa4c2315749016e49274463
Rene Weber <weber.rene@live.de>
2019-07-28 01:15:29 +0100

    Improve validation result interface

--- src/main/java/ValidationResult.java
+++ src/main/java/ValidationResult.java
@@ -1,19 +1,21 @@
-import java.util.Optional;
-
 public class ValidationResult<T> {
   private final T value;
-  private final Optional<String> message;
+  private final String message;
 
-  public ValidationResult(final T value, final Optional<String> message) {
+  private ValidationResult(final T value, final String message) {
     this.value = value;
     this.message = message;
+  }
+
+  public static <T> ValidationResult<T> of(final T value, final String message) {
+    return new ValidationResult<>(value, message);
   }
 
   public T getValue() {
     return value;
   }
 
-  public Optional<String> getMessage() {
+  public String getMessage() {
     return message;
   }
 }
