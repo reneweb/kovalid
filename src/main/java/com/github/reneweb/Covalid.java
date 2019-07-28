8f75b6b6d96675186585041e2e704de63c898738
Rene Weber <weber.rene@live.de>
2019-07-28 20:07:58 +0100

    covalid extends validators

--- src/main/java/com/github/reneweb/Covalid.java
+++ src/main/java/com/github/reneweb/Covalid.java
@@ -1,10 +1,14 @@
 package com.github.reneweb;
 
+import com.github.reneweb.validators.DoubleValidators;
+import com.github.reneweb.validators.FloatValidators;
 import com.github.reneweb.validators.IntValidators;
+import com.github.reneweb.validators.LongValidators;
 import com.github.reneweb.validators.ObjectValidators;
 import com.github.reneweb.validators.StringValidators;
 
-public interface Covalid<I, O> extends ObjectValidators, StringValidators, IntValidators {
+public interface Covalid<I, O> extends ObjectValidators, StringValidators, IntValidators,
+                                       LongValidators, DoubleValidators, FloatValidators {
 
   ValidationResult<O> validate(I input);
 }
