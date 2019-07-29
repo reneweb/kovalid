522c06f0fed75bd7bd43f6469393c9d0f721a1e4
Rene Weber <weber.rene@live.de>
2019-07-30 00:34:26 +0100

    add float validators test

---
+++ src/test/java/com/github/reneweb/validators/FloatValidatorsTest.java
@@ -0,0 +1,81 @@
+package com.github.reneweb.validators;
+
+import com.github.reneweb.ValidationResult;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FloatValidatorsTest {
+  @Test
+  public void isEqual() {
+    Float value = 1.2F;
+
+    FloatValidators dv = new FloatValidators() {};
+    ValidationResult<Float> resultSuccess = dv.isEqual(value, 1.2F);
+    ValidationResult<Float> resultSuccessCustomMessage = dv.isEqual(value, 1.2F, "custom");
+
+    ValidationResult<Float> resultFailure = dv.isEqual(value, 3.1F);
+    ValidationResult<Float> resultFailureCustomMessage = dv.isEqual(value, 3.1F, "custom");
+
+    assertThat(resultSuccess.isValid()).isTrue();
+    assertThat(resultSuccess.getValue()).isEqualTo(value);
+    assertThat(resultSuccessCustomMessage.isValid()).isTrue();
+    assertThat(resultSuccessCustomMessage.getMessage()).isNull();
+    assertThat(resultSuccessCustomMessage.getValue()).isEqualTo(value);
+
+    assertThat(resultFailure.isValid()).isFalse();
+    assertThat(resultFailure.getValue()).isEqualTo(value);
+    assertThat(resultFailureCustomMessage.isValid()).isFalse();
+    assertThat(resultFailureCustomMessage.getMessage()).isEqualTo("custom");
+    assertThat(resultFailureCustomMessage.getValue()).isEqualTo(value);
+  }
+
+  @Test
+  public void isGreaterThan() {
+    Float value = 1.2F;
+
+    FloatValidators dv = new FloatValidators() {};
+    ValidationResult<Float> resultSuccess = dv.isGreaterThan(value, 1.1F);
+    ValidationResult<Float> resultSuccessCustomMessage = dv.isGreaterThan(value, 1.1F, "custom");
+
+    ValidationResult<Float> resultFailure = dv.isGreaterThan(value, 1.2F);
+    ValidationResult<Float> resultFailureCustomMessage = dv.isGreaterThan(value, 1.2F, "custom");
+
+    assertThat(resultSuccess.isValid()).isTrue();
+    assertThat(resultSuccess.getValue()).isEqualTo(value);
+    assertThat(resultSuccessCustomMessage.isValid()).isTrue();
+    assertThat(resultSuccessCustomMessage.getMessage()).isNull();
+    assertThat(resultSuccessCustomMessage.getValue()).isEqualTo(value);
+
+    assertThat(resultFailure.isValid()).isFalse();
+    assertThat(resultFailure.getValue()).isEqualTo(value);
+    assertThat(resultFailureCustomMessage.isValid()).isFalse();
+    assertThat(resultFailureCustomMessage.getMessage()).isEqualTo("custom");
+    assertThat(resultFailureCustomMessage.getValue()).isEqualTo(value);
+  }
+
+  @Test
+  public void isSmallerThan() {
+    Float value = 1.2F;
+
+    FloatValidators dv = new FloatValidators() {};
+    ValidationResult<Float> resultSuccess = dv.isSmallerThan(value, 1.3F);
+    ValidationResult<Float> resultSuccessCustomMessage = dv.isSmallerThan(value, 1.3F, "custom");
+
+    ValidationResult<Float> resultFailure = dv.isSmallerThan(value, 1.2F);
+    ValidationResult<Float> resultFailureCustomMessage = dv.isSmallerThan(value, 1.2F, "custom");
+
+    assertThat(resultSuccess.isValid()).isTrue();
+    assertThat(resultSuccess.getValue()).isEqualTo(value);
+    assertThat(resultSuccessCustomMessage.isValid()).isTrue();
+    assertThat(resultSuccessCustomMessage.getMessage()).isNull();
+    assertThat(resultSuccessCustomMessage.getValue()).isEqualTo(value);
+
+    assertThat(resultFailure.isValid()).isFalse();
+    assertThat(resultFailure.getValue()).isEqualTo(value);
+    assertThat(resultFailureCustomMessage.isValid()).isFalse();
+    assertThat(resultFailureCustomMessage.getMessage()).isEqualTo("custom");
+    assertThat(resultFailureCustomMessage.getValue()).isEqualTo(value);
+  }
+}
