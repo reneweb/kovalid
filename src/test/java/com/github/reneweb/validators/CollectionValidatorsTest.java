495ebf75ba34b2baec62e98ac5aef57ef1ecb0d4
Rene Weber <weber.rene@live.de>
2019-07-30 00:25:42 +0100

    add test for collection containsElement

--- src/test/java/com/github/reneweb/validators/CollectionValidatorsTest.java
+++ src/test/java/com/github/reneweb/validators/CollectionValidatorsTest.java
@@ -34,4 +34,28 @@ class CollectionValidatorsTest {
     assertThat(resultFailureCustomMessage.getMessage()).isEqualTo("custom");
     assertThat(resultFailureCustomMessage.getValue()).isEqualTo(list);
   }
+
+  @Test
+  public void containsElement() {
+    List<String> list = Arrays.asList("first", "second");
+
+    CollectionValidators cv = new CollectionValidators() {};
+    ValidationResult<List<String>> resultSuccess = cv.containsElement(list, "second");
+    ValidationResult<List<String>> resultSuccessCustomMessage = cv.containsElement(list, "second", "custom");
+
+    ValidationResult<List<String>> resultFailure = cv.containsElement(list, "non-existent");
+    ValidationResult<List<String>> resultFailureCustomMessage = cv.containsElement(list, "non-existent", "custom");
+
+    assertThat(resultSuccess.isValid()).isTrue();
+    assertThat(resultSuccess.getValue()).isEqualTo(list);
+    assertThat(resultSuccessCustomMessage.isValid()).isTrue();
+    assertThat(resultSuccessCustomMessage.getMessage()).isNull();
+    assertThat(resultSuccessCustomMessage.getValue()).isEqualTo(list);
+
+    assertThat(resultFailure.isValid()).isFalse();
+    assertThat(resultFailure.getValue()).isEqualTo(list);
+    assertThat(resultFailureCustomMessage.isValid()).isFalse();
+    assertThat(resultFailureCustomMessage.getMessage()).isEqualTo("custom");
+    assertThat(resultFailureCustomMessage.getValue()).isEqualTo(list);
+  }
 }
